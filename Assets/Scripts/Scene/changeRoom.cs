41d69f6de6a588a0037d082ff9fb35d303d7f539
Sixyears6 <prokhanh73@gmail.com>
2026-05-26 18:53:22 +0700

    resolve conflict Bangers SDF.asset

--- Assets/Scripts/Scene/changeRoom.cs
+++ Assets/Scripts/Scene/changeRoom.cs
@@ -5,8 +5,18 @@ public class changeRoom : MonoBehaviour
 {
     public string senceToLoad;
 
+    // Dùng cho BUTTON (click)
     public void ChangeSceneShow()
     {
         SceneManager.LoadScene(senceToLoad);
+    }
+
+    // Dùng cho TRIGGER (chạm vào)
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(senceToLoad);
+        }
     }
 }
