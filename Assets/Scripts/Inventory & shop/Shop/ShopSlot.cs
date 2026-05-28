559772e4e33d70d05390f25aa56b6452bebb45f9
AnHong541 <an.honglhu@gmail.com>
2026-05-28 08:10:02 +0700

    sua shop

--- Assets/Scripts/Inventory & shop/Shop/ShopSlot.cs
+++ Assets/Scripts/Inventory & shop/Shop/ShopSlot.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
-public class ShopSlot : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
+public class ShopSlot : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler, IPointerMoveHandler
 {
     public ItemSO itemSO;
     public TMP_Text itemNameText;
@@ -46,5 +46,11 @@ public class ShopSlot : MonoBehaviour , IPointerEnterHandler, IPointerExitHandle
     public void OnPointerExit(PointerEventData eventData)
     {
         shopInfo.HideItemInfo();
+    }
+
+    public void OnPointerMove(PointerEventData eventData)
+    {
+        if (itemSO != null)
+            shopInfo.FollowMouse();
     }
 }
